a8327cf1a2d144305724a74a139f13777903bbda
Alejandro Zepeda <alexzepeda@hotmail.com>
2020-11-11 22:28:39 -0600

    Práctica 08 - RMI

---
+++ Practice08/Imagenes.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.util.*;
+import java.rmi.*;
+
+
+public interface Imagenes extends Remote {
+	ImageIcon getImage(String imageID) throws RemoteException, Exception;
+	Vector getNombres() throws java.rmi.RemoteException;
+	void setIma(String nombre , ImageIcon ima) throws RemoteException, Exception;
+}
